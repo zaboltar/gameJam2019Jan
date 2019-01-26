b53b9aa995915dd6a8afe0d49afa88f789d3ebb1
Joe Cordova <johelyn.cordova@gmai.com>
2019-01-26 12:46:53 -0500

    base new proj

---
+++ Aoa´s Quest/Assets/PlayerManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public float maxHealth, maxThirst, maxHunger;
+    public float thirstIncreaseRate, hungerIncreaseRate;
+    private float health, thirst, hunger;
+
+    public bool playerIsDead;
+
+    public void Start() {
+        health = maxHealth;
+    }
+
+    public void Update() {
+        // thirst and hunger increases
+
+       
+
+        if (!playerIsDead)
+        {
+            hunger += hungerIncreaseRate * Time.deltaTime;
+            thirst += thirstIncreaseRate * Time.deltaTime;
+        }
+
+
+        if (thirst >= maxThirst)
+        {
+            Die();
+        }
+
+        if (hunger >= maxHunger)
+        {
+            Die();
+        }
+
+    }
+
+    public void Die() {
+        // Death mechanics and logic here
+        // Valar Morghulis
+        playerIsDead = true;
+    }
+
+}
---
+++ Aoa´s Quest/Assets/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMove : MonoBehaviour {
+
+    public LayerMask whatCanBeClickedOn;
+
+    private NavMeshAgent myAgent;
+
+	// Use this for initialization
+	void Start () {
+        myAgent = GetComponent<NavMeshAgent>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+            if ( Input.GetMouseButtonDown (0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+                
+            if (Physics.Raycast (myRay, out hitInfo, 100, whatCanBeClickedOn)) {
+
+                myAgent.SetDestination(hitInfo.point);
+
+            }
+        }
+
+	}
+}
